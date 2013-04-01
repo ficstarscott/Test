6a56d47cbba8f53605cbdc456cbefc5c5256cbb1
Scott Vahey <scott@ficstar.com>
2013-04-01 09:27:26 -0400

    First commit from GIT GUI

---
+++ Test Application/Test Application/Test Application/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Test_Application
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxWebsite = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxWebsite
+            // 
+            this.comboBoxWebsite.FormattingEnabled = true;
+            this.comboBoxWebsite.Location = new System.Drawing.Point(49, 25);
+            this.comboBoxWebsite.Name = "comboBoxWebsite";
+            this.comboBoxWebsite.Size = new System.Drawing.Size(152, 21);
+            this.comboBoxWebsite.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.comboBoxWebsite);
+            this.Name = "frmMain";
+            this.Text = "GIT Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxWebsite;
+    }
+}
+
